9c9eaff51ccb04fa3edbc323aa5a4fb486d21ea1
Dikhsantibong1432 <dwiranggatibongdikhsan@gmail.com>
2024-06-16 20:19:37 +0800

    perbaikan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -91,7 +91,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-6 about-img">
-            <img src="{{ asset('landing-page/img/about-img.jpg') }}" alt="">
+            <img src="{{ asset('landing-page/img/about.png') }}" alt="">
           </div>
 
           <div class="col-lg-6 content">
@@ -380,81 +380,87 @@
     <!--==========================
       Our Team Section
     ============================-->
-    <section id="team" class="wow fadeInUp">
-      <div class="container">
-        <div class="section-header">
-          <h2>Our Team</h2>
-          <p>The Creative Tech team consists of dedicated and experienced professionals in their respective fields, ready to help you achieve your digital goals.</p>
-        </div>
-        <div class="row">
-          <div class="col-lg-3 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="{{ asset('landing-page/img/diki.jpg')}}" alt=""></div>
-              <div class="details">
-                <h4>Diki Ardika</h4>
-                <span>Programmer</span>
-                <div class="social">
-                  <a href=""><i class="fa fa-twitter"></i></a>
-                  <a href=""><i class="fa fa-facebook"></i></a>
-                  <a href=""><i class="fa fa-google-plus"></i></a>
-                  <a href=""><i class="fa fa-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="{{ asset('landing-page/img/pais.jpg')}}" alt=""></div>
-              <div class="details">
-                <h4>Muhammad Faiz</h4>
-                <span>Marketing</span>
-                <div class="social">
-                  <a href=""><i class="fa fa-twitter"></i></a>
-                  <a href=""><i class="fa fa-facebook"></i></a>
-                  <a href=""><i class="fa fa-google-plus"></i></a>
-                  <a href=""><i class="fa fa-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="{{ asset('landing-page/img/febri2.png')}}" alt=""></div>
-              <div class="details">
-                <h4>Febri NB</h4>
-                <span>UI UX Designer</span>
-                <div class="social">
-                  <a href=""><i class="fa fa-twitter"></i></a>
-                  <a href=""><i class="fa fa-facebook"></i></a>
-                  <a href=""><i class="fa fa-google-plus"></i></a>
-                  <a href=""><i class="fa fa-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="{{ asset('landing-page/img/diksan.jpg')}}" alt=""></div>
-              <div class="details">
-                <h4>Dikhsan Dwirangga</h4>
-                <span>Programmer</span>
-                <div class="social">
-                  <a href=""><i class="fa fa-twitter"></i></a>
-                  <a href=""><i class="fa fa-facebook"></i></a>
-                  <a href=""><i class="fa fa-google-plus"></i></a>
-                  <a href=""><i class="fa fa-linkedin"></i></a>
-                </div>
+<section id="team" class="wow fadeInUp">
+  <div class="container">
+    <div class="section-header">
+      <h2>Our Team</h2>
+      <p>The Creative Tech team consists of dedicated and experienced professionals in their respective fields, ready to help you achieve your digital goals.</p>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <div class="pic"><img src="{{ asset('landing-page/img/diki.jpg')}}" alt="" class="img-fluid"></div>
+            <div class="details">
+              <h4>Diki Ardika</h4>
+              <span>Programmer</span>
+              <div class="social">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-google-plus"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
               </div>
             </div>
           </div>
         </div>
-
       </div>
-    </section><!-- #team -->
 
+      <div class="col-lg-3 col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <div class="pic"><img src="{{ asset('landing-page/img/pais.jpg')}}" alt="" class="img-fluid"></div>
+            <div class="details">
+              <h4>Muhammad Faiz</h4>
+              <span>Marketing</span>
+              <div class="social">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-google-plus"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <div class="pic"><img src="{{ asset('landing-page/img/febri2.png')}}" alt="" class="img-fluid"></div>
+            <div class="details">
+              <h4>Febri NB</h4>
+              <span>UI UX Designer</span>
+              <div class="social">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-google-plus"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <div class="pic"><img src="{{ asset('landing-page/img/diksan.jpg')}}" alt="" class="img-fluid"></div>
+            <div class="details">
+              <h4>Dikhsan Dwirangga</h4>
+              <span>Programmer</span>
+              <div class="social">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-google-plus"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section><!-- #team -->
     <!--==========================
       Contact Section
     ============================-->
